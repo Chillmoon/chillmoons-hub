e0b7e54ee4b1cf238b701cf6231220aea2a3a3ed
Chillmoon <Chillmoon07@gmail.com>
2024-11-24 19:59:25 +0200

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://Chillmoon:${process.env.MONGOPASS}@chillmoons-hub.5a7tv.mongodb.net/chillmoons-hub?retryWrites=true&w=majority&appName=chillmoons-hub`
+  )
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((e) => {
+    console.log(e.errorResponse.errmsg);
+  });
 
 const app = express();
 
