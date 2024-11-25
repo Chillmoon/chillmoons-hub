78130026c2be4517ca621dee126b0ed1fd2b8062
Chillmoon <Chillmoon07@gmail.com>
2024-11-25 14:47:30 +0200

    created functionality for signin page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
-import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.model.js";
+
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
@@ -16,6 +19,44 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("Signup is successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const foundUser = await User.findOne({ email });
+    if (!foundUser) {
+      next(errorHandler(404, "User not found"));
+    }
+
+    const validPassport = bcryptjs.compareSync(password, foundUser.password);
+    if (!validPassport) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign(
+      {
+        id: foundUser._id,
+      },
+      process.env.JWT_SECRET
+    );
+
+    const { password: pass, ...rest } = foundUser._doc;
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest.username + ", Signin successful");
   } catch (error) {
     next(error);
   }
