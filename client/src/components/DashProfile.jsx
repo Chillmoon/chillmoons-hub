bd4fab24c91de2f78f3cd38a234854842b476ceb
Chillmoon <Chillmoon07@gmail.com>
2024-11-26 20:59:56 +0200

    added functionality for uploading the profile picture

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,20 +1,118 @@
-import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileURL, setImageFileURL] = useState(null);
+  const [uploadingProgress, setUploadingProgress] = useState(null);
+  const [uploadingError, setUploadingError] = useState(null);
+
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileURL(URL.createObjectURL(file));
+    }
+  };
+
+  const uploadImage = async () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+
+    setUploadingError(null);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadingProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setUploadingError(
+          "Sorry, we could not upload image. File must be less than 2Mb"
+        );
+        setUploadingProgress(null);
+        setImageFile(null);
+        setImageFileURL(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileURL(downloadURL);
+        });
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"
+          onClick={() => {
+            filePickerRef.current.click();
+          }}
+        >
+          {uploadingProgress && (
+            <CircularProgressbar
+              value={uploadingProgress || 0}
+              text={`${uploadingProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: "teal",
+                },
+              }}
+            />
+          )}
+
           <img
-            src={currentUser.profilePicture}
+            src={imageFileURL || currentUser.profilePicture}
             alt="profile"
-            className="rounded-full w-full h-full object-cover border-8 border-[teal]"
+            className={`rounded-full w-full h-full object-cover border-8 border-[teal] ${
+              uploadingProgress && uploadingProgress < 100 && "opacity-50"
+            }`}
           />
         </div>
+        {uploadingError && <Alert color="failure">{uploadingError}</Alert>}
+
         <TextInput
           type="text"
           id="username"
