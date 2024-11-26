91b38ac2eadb7e6bf67242b11c6331debb53b40b
Chillmoon <Chillmoon07@gmail.com>
2024-11-26 17:59:07 +0200

    added private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,18 +10,21 @@ import Dashboard from "./pages/Dashboard";
 
 import Header from "./components/Header";
 import CustomFooter from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <CustomFooter />
     </BrowserRouter>
