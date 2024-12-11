b6f7282f89039a1295c9421d7afdec7592180a43
Chillmoon <Chillmoon07@gmail.com>
2024-12-11 22:18:54 +0200

    prepare for deploy

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -23,6 +24,8 @@ mongoose
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use(cookieParser());
@@ -37,6 +40,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
